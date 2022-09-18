102fe1ff94a45f6f26d5609bddee5e6360a98a90
a.ponomarev <AAPonomarjov@yandex.ru>
2022-09-18 13:47:45 +0300

    Add test

---
+++ JenkinsTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace JenkinsTest
+{
+    [TestFixture]
+    class SimpleTest
+    {
+        [Test]
+        public void TestMethod()
+        {
+            Assert.IsTrue(3 == 3, "3 is not 3");
+        }
+    }
+}
